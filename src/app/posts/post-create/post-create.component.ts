c550e86ba7bf19e922c7832810876da241eb8e10
akash <akash.ry@somaiya.edu>
2020-11-19 19:15:09 +0530

    mime-type validator to image upload

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -6,6 +6,7 @@ import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
 import { timestamp } from 'rxjs/operators';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -35,7 +36,9 @@ export class PostCreateComponent implements OnInit{
     this.form = new FormGroup({
       'title': new FormControl(null, {validators:[Validators.required, Validators.minLength(3)]}),
       'content': new FormControl(null, {validators:[Validators.required]}),
-      image: new FormControl(null, {validators: [Validators.required]})
+      image: new FormControl(null, {
+         validators: [Validators.required],
+         asyncValidators: [mimeType]})
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap)=>{

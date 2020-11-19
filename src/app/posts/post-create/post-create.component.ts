eaf854b6b647ede75e1088ec46c4826b6d3c0b1b
akash <akash.ry@somaiya.edu>
2020-11-19 16:23:05 +0530

    view image preview while uploading image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -19,7 +19,8 @@ export class PostCreateComponent implements OnInit{
   enteredContent = '';
   post: Post;
   isLoading = false;
-  form: FormGroup
+  form: FormGroup;
+  imgPreview;
 
   private mode = 'create';
   private postId = '';
@@ -33,7 +34,8 @@ export class PostCreateComponent implements OnInit{
   ngOnInit(){
     this.form = new FormGroup({
       'title': new FormControl(null, {validators:[Validators.required, Validators.minLength(3)]}),
-      'content': new FormControl(null, {validators:[Validators.required]})
+      'content': new FormControl(null, {validators:[Validators.required]}),
+      image: new FormControl(null, {validators: [Validators.required]})
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap)=>{
@@ -55,6 +57,18 @@ export class PostCreateComponent implements OnInit{
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imgPreview = reader.result;
+    };
+    reader.readAsDataURL(file);
+
   }
 
   onSavePost(){
